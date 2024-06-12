35fe5128cf3e213667d1e0f3cdb7548d36a7495d
SumedhThota <sumedharya569@gmail.com>
2024-06-11 23:22:21 -0400

    Receiving Token from Spotify

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 import Login from './Login.js'
+import React, {useEffect} from 'react';
+import { getTokenFromUrl } from './Spotify.js';
 
 function App() {
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log("I have a token", token)
+  }, [])
   return (
     <div className="App">
       <Login />
