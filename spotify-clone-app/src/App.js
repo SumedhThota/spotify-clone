4688b65f2d7c2d299aefcf7ba20304576022a6c6
SumedhThota <sumedharya569@gmail.com>
2024-06-13 13:20:12 -0400

    Getting user details using API

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import Login from './Login.js'
 import React, {useEffect, useState} from 'react';
 import { getTokenFromUrl } from './spotify.js';
+import SpotifyWebApi from "spotify-web-api-js"
 
 function App() {
+  const spotify = new SpotifyWebApi ()
   const [token, setToken] = useState(null)
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -12,6 +14,10 @@ function App() {
     const _token = hash.access_token
     if(_token){
       setToken(_token)
+      spotify.setAccessToken(_token)
+      spotify.getMe().then(user =>{
+        console.log(user)
+      })
     }
   }, [])
   return (
