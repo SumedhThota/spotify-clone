7cb497ba72b2e8840a524b1782811ab944cacb36
SumedhThota <sumedharya569@gmail.com>
2024-06-17 13:39:11 -0400

    Setting up Context API

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import Login from './Login.js'
 import React, {useEffect, useState} from 'react';
 import { getTokenFromUrl } from './spotify.js';
-import SpotifyWebApi from "spotify-web-api-js"
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player.js';
+import { useDataLayerValue } from './DataLayer.js';
 
+const spotify = new SpotifyWebApi()
 function App() {
-  const spotify = new SpotifyWebApi ()
+  
   const [token, setToken] = useState(null)
+  const [{user}, dispatch] = useDataLayerValue()
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = ""
@@ -14,17 +18,22 @@ function App() {
     const _token = hash.access_token
     if(_token){
       setToken(_token)
+      
       spotify.setAccessToken(_token)
       spotify.getMe().then(user =>{
-        console.log(user)
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
       })
     }
   }, [])
+  console.log(user)
   return (
     <div className="App">
       {
         token?
-        (<h1>I am logged in</h1>):
+        (<Player />):
         (<Login />)
       }
       
